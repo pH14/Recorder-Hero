cd2fc3a46160c8a15f25b757d5e4563699832e3a
Paul W Hemberger <pwh@mit.edu>
2012-11-26 17:48:02 -0500

    rough music loader demo, full scale + accidentals, colors for types of notes

--- src/pwh_lab/rh_video_display/picture_blob.v
+++ src/pwh_lab/rh_video_display/picture_blob.v
@@ -16,21 +16,17 @@ module picture_blob
 	
 	wire [11:0] image_adr;
 	// num of bits for 128*256 ROM
-	wire [7:0] image_bits, red_mapped,
-	green_mapped, blue_mapped;
+	wire [7:0] image_bits;
 	// note the one clock cycle delay in pixel!
 	always @ (posedge pixel_clk) begin
 		if ((hcount >= x && hcount < (x+WIDTH)) &&
 			(vcount >= y && vcount < (y+HEIGHT)))
-				pixel <= {red_mapped, green_mapped, blue_mapped};
-		else pixel = 0;
+				pixel <= {3{image_bits}};
+		else pixel <= 0;
 	end
+	
 	// calculate rom address and read the location
 	assign image_addr = (hcount-x) + (vcount-y) * WIDTH;
-	image_rom note_letters(image_addr, clock_65mhz, image_bits);
-	// use color map to create 8bits R, 8bits G, 8 bits B;
-	red_color_map rcm (image_bits, pixel_clk, red_mapped);
-	green_color_map gcm (image_bits, pixel_clk.green_mapped);
-	blue_color_map bcm (image_bits, pixel_clk, blue_mapped);
+	note_letters nl(.addra(image_addr), .clka(clock_65mhz), .douta(image_bits));
 endmodule
 
